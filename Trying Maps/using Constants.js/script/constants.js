818582ee4fda3870ecaa4ccb1807d40ab31d8071
ShashankKumarNITT <83637916+ShashankKumarNITT@users.noreply.github.com>
2023-06-28 21:55:39 +0530

    Add files via upload

---
+++ Trying Maps/using Constants.js/script/constants.js
@@ -0,0 +1,88 @@
+ 
+const CRIME_DATA = [
+    {
+      "lat": 10.780128396223796,
+      "long": 78.67321419460255,
+      "location": "E.PUDUR",
+      "SUM(Accused)": 23,
+      "amount": 8.459999999999999
+    },
+    {
+      "lat": 10.825836937361702, 
+      "long": 78.67322647219542,
+      "location": "Woraiyur",
+      "SUM(Accused)": 9,
+      "amount": 4.000000000000001
+    },
+    {
+      "lat": 10.80921441201769, 
+      "long": 78.68944517889851,
+      "location": "PEW",
+      "SUM(Accused)": 7,
+      "amount": 2.467
+    },
+    {
+      "lat": 10.808854374671592,
+      "long": 78.68498180827136,
+      "location": "S.COURT",
+      "SUM(Accused)": 1,
+      "amount": 0.71
+    },
+    {
+      "lat": 10.824795165273903,
+      "long": 78.69604643710744,
+      "location": "FORT",
+      "SUM(Accused)": 13,
+      "amount": 3.000000000000001
+    },
+    {
+      "lat": 10.812596382766396, 
+      "long": 78.69704624867468,
+      "location": "Palakkarai",
+      "SUM(Accused)": 14,
+      "amount": 5.049999999999999
+    },
+    {
+      "lat": 10.85749158239768,
+      "long": 78.68971475060026,
+      "location": "Srirangam",
+      "SUM(Accused)": 4,
+      "amount": 0.21000000000000002
+    },
+    {
+      "lat": 10.817144954913905,
+      "long": 78.69430463229762,
+      "location": "G.Market",
+      "SUM(Accused)": 9,
+      "amount": 3
+    },
+    {
+      "lat": 10.814482099293738,
+      "long": 78.7196088057719,
+      "location": "A.MANGALAM",
+      "SUM(Accused)": 3,
+      "amount": 1.1
+    },
+    {
+      "lat": 10.75734109532593,
+      "long": 78.68881885059899,
+      "location": "K.K. NAGAR",
+      "SUM(Accused)": 1,
+      "amount": 0.01
+    },
+    {
+      "lat": 10.821123225839905,
+      "long": 78.68630436202663,
+      "location": "T.NAGER",
+      "SUM(Accused)": 1,
+      "amount": 0.05
+    },
+    {
+      "lat": 10.759111076970328,
+      "long": 78.7076749929273,
+      "location": "Airport",
+      "SUM(Accused)": 1,
+      "amount": 0.15
+    }
+  ];
+  
