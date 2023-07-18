df66b2e416eea63ea0ee84d15325e87b80e8b387
pravsss <pravalikamekala31@gmail.com>
2023-07-18 15:34:24 +0530

    added dashboard

---
+++ frontend/Graphs.js
@@ -0,0 +1,43 @@
+let currentIndex = 0;
+const images = document.querySelectorAll('.slider img');
+const dots = document.querySelectorAll('.dot');
+const totalImages = images.length;
+
+function showImage(index) {
+  images.forEach((image, i) => {
+    if (i === index) {
+      image.style.display = 'block';
+    } else {
+      image.style.display = 'none';
+    }
+  });
+  dots.forEach((dot, i) => {
+    if (i === index) {
+      dot.classList.add('active');
+    } else {
+      dot.classList.remove('active');
+   }
+  });
+}
+
+function nextImage() {
+  currentIndex++;
+  if (currentIndex >= totalImages) {
+    currentIndex = 0;
+  }
+  showImage(currentIndex);
+}
+dots.forEach((dot,i)=>{
+    dot.addEventListener('click',()=>{
+        currentIndex = i;
+        showImage(currentIndex);
+    });
+});
+setInterval(nextImage, 2000); // Change image every 2 seconds
+showImage(currentIndex);
+function toggleSidebar() {
+    const sidebar = document.querySelector('.sidebar');
+    const content = document.querySelector('.content');
+    sidebar.classList.toggle('open');
+    content.classList.toggle('open');
+  }
